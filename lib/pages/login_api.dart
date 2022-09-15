7672b3a86d40ce03defc026c268b4fec1e856c43
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-09-15 19:33:22 -0300

    CorrecaoErroLogin

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -1,38 +1,43 @@
 import 'dart:convert';
-import 'dart:developer';
-
 import 'package:carros/pages/api_response.dart';
 import 'package:carros/pages/usuario.dart';
-import "package:http/http.dart" as http;
+import 'package:http/http.dart' as http;
 
 class LoginApi {
   static Future<ApiResponse<Usuario>> login(String login, String senha) async {
-    var url = 'http://carros-springboot.herokuapp.com/api/v2/login';
+    try {
+      var url = 'https://carros-springboot.herokuapp.com/api/v2/login';
 
-    Map<String, String> headers = {
-      "Content-Type": "application/json",
-    };
+      Map<String,String> headers = {
+        "Content-Type": "application/json"
+      };
 
-    Map params = {
-      "username": "login",
-      "password": "senha"
-    };
+      Map params = {
+        "username": login,
+        "password": senha
+      };
 
-    String s = jsonEncode(params);
-    print(">>> $s");
+      String s = json.encode(params);
+      print(">> $s");
 
-    var response = await http.post(url, body: s, headers: headers);
+      var response = await http.post(url, body: s, headers: headers);
 
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
 
-    Map mapResponse = json.decode(response.body);
+      Map mapResponse = json.decode(response.body);
 
-    if (response.statusCode == 200) {
-      final user = Usuario.fromjson(mapResponse);
+      if(response.statusCode == 200) {
+        final user = Usuario.fromjson(mapResponse);
 
-      return ApiResponse.ok(user);
-    }
+        return ApiResponse.ok(user);
+      }
+
       return ApiResponse.error(mapResponse["error"]);
+    } catch(error, exception) {
+      print("Erro no login $error > $exception");
+
+      return ApiResponse.error("Não foi possível fazer o login.");
+    }
   }
 }
