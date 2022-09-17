73363c81bf9d41146543ad2e62e3c04382205abb
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-09-17 16:05:16 -0300

    Implementação do FutueBuilder na classe de home_page metodo _listView

--- lib/pages/carro/carros_api.dart
+++ lib/pages/carro/carros_api.dart
@@ -2,8 +2,10 @@
 import 'carro.dart';
 
 class CarrosApi {
-  static List<Carro> getCarros() {
+  static Future<List<Carro>> getCarros() async {
     final carros = List<Carro>();
+
+    await Future.delayed(Duration(seconds: 2));
 
     carros.add(Carro(nome: "Renault Megane RS Trophi2", urlFoto: "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/Renault_Megane_Trophy.png"));
     carros.add(Carro(nome: "Ferrari FF", urlFoto: "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/Maserati_Grancabrio_Sport.png"));
--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'carro.dart';
 
 class HomePage extends StatelessWidget {
+  get future => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,29 +19,80 @@ class HomePage extends StatelessWidget {
   }
 
   _body() {
-    List<Carro> carros = CarrosApi.getCarros();
+    Future<List<Carro>> future = CarrosApi.getCarros();
 
-    return ListView.builder(
-        itemCount: carros.length,
+    return FutureBuilder(
+      future: future,
+      builder: (context, snapshot) {
+
+      if (! snapshot.hasData) {
+        return Center(child: CircularProgressIndicator(),);
+      }
+
+        List<Carro> carros = snapshot.data;
+        return _listView(carros);
+      },
+    );
+  }
+
+  Container _listView(List<Carro> carros) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView.builder(
+        itemCount: carros != null ? carros.length : 0,
         itemBuilder: (context, index) {
           Carro c = carros[index];
 
-          return Row(
-            children: <Widget>[
-              Image.network(
-                c.urlFoto,
-                width: 150,
+          return Card(
+            color: Colors.grey[200],
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Image.network(
+                      c.urlFoto,
+                      width: 250,
+                    ),
+                  ),
+                  Text(
+                    c.nome,
+                    maxLines: 1,
+                    /*Conteudo deve se auto adaptar no container do Flexible em uma linha cada objeto*/
+                    overflow: TextOverflow.ellipsis,
+                    /*Define os tres pontinhos na descrição quando não cabe na widget do Flexible*/
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    "Descrição...",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  ButtonBarTheme(
+                    data: ButtonBarTheme.of(context),
+                    child: ButtonBar(
+                      children: <Widget>[
+                        FlatButton(
+                          child: const Text('DETALHES'),
+                          onPressed: () {
+                            /* ... */
+                          },
+                        ),
+                        FlatButton(
+                          child: const Text('SHARE'),
+                          onPressed: () {
+                            /* ... */
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Flexible( /*Realiza o ajuste do conteudo automaticamente na tela, não estourando o espaço.(Ficando amarelo)*/
-                child: Text(
-                  c.nome,
-                  maxLines: 1, /*Conteudo deve se auto adaptar no container do Flexible em uma linha cada objeto*/
-                  overflow: TextOverflow.ellipsis, /*Define os tres pontinhos na descrição quando não cabe na widget do Flexible*/
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ],
+            ),
           );
-        });
+        },
+      ),
+    );
   }
 }
