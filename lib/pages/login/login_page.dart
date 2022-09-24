186209641253d32fb13957babeadce0badc11b99
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-09-24 13:28:40 -0300

    Bloc - Login

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -3,12 +3,15 @@ import 'dart:async';
 import 'package:carros/pages/api_response.dart';
 import 'package:carros/pages/carro/home_page.dart';
 import 'package:carros/pages/login/login_api.dart';
+import 'package:carros/pages/login/login_bloc.dart';
 import 'package:carros/pages/login/usuario.dart';
 import 'package:carros/utils/alert.dart';
 import 'package:carros/utils/nav.dart';
 import 'package:carros/widgets/app_button.dart';
 import 'package:carros/widgets/app_text.dart';
 import 'package:flutter/material.dart';
+
+import 'login_api.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -17,7 +20,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _streamController = StreamController<bool>();
+
+  final _bloc = LoginBloc();
 
   final _tlogin = TextEditingController();
 
@@ -78,7 +82,7 @@ class _LoginPageState extends State<LoginPage> {
               height: 20,
             ),
             StreamBuilder<bool>(
-              stream: _streamController.stream, /*Se "Conecta a variavel _streamControllr"*/
+              stream: _bloc.stream, /*Se "Conecta a variavel _streamControllr"*/
               builder: (context, snapshot) {
                 return AppButton(
                   "Login",
@@ -104,9 +108,7 @@ class _LoginPageState extends State<LoginPage> {
 
     print("Login: $login, senha: $senha");
 
-    _streamController.add(true);
-
-    ApiResponse response = await LoginApi.login(login, senha);
+    ApiResponse response = await _bloc.login(login, senha);
 
     if (response.ok) {
       Usuario user = response.result;
@@ -117,8 +119,6 @@ class _LoginPageState extends State<LoginPage> {
     } else {
       alert(context, response.msg);
     }
-
-    _streamController.add(false);
 
     setState(() {
       _showProgress = false;
@@ -144,8 +144,7 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
-    _streamController.close();
+    _bloc.dispose();
   }
 }
