e0272bcc5b34f5a45b80a70072ce7f6c69f0d10d
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-09-26 16:45:53 -0300

    133 - Bloc - Herança

--- lib/pages/login/login_bloc.dart
+++ lib/pages/login/login_bloc.dart
@@ -1,28 +1,23 @@
 
 import 'dart:async';
 
+import 'package:carros/pages/carro/simple_bloc.dart';
 import 'package:carros/pages/login/usuario.dart';
 import '../api_response.dart';
 import 'login_api.dart';
 
-class LoginBloc {
-  final _streamController = StreamController<bool>();
-
-  get stream => _streamController.stream;
+class LoginBloc extends SimpleBloc<bool> {
 
   Future<ApiResponse<Usuario>> login(String login, String senha) async {
 
-    _streamController.add(true);
+    add(true);
 
     ApiResponse response = await LoginApi.login(login, senha);
 
-    _streamController.add(false);
+    add(false);
 
     return response;
 
   }
 
-  void dispose(){
-    _streamController.close();
-  }
 }
