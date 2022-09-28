de1af259779df10aec915faa15a3c7ce98f88886
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-09-28 16:20:27 -0300

    implementação Actions: IconButton e PopupMenuButton em carro_page

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -11,9 +11,27 @@ class CarroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
         appBar: AppBar(
           title: Text(carro.nome),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.place),
+              onPressed: _onClickMapa,
+            ),
+            IconButton(
+              icon: Icon(Icons.videocam),
+              onPressed: _onClickVideo,
+            ),
+            PopupMenuButton<String>(
+              onSelected: (String value) => _onClickPopupMenu(value),
+              itemBuilder: (BuildContext context) {
+                return [PopupMenuItem(value: "Editar" ,child: Text("Editar"),),
+                PopupMenuItem(value: "Deletar" ,child: Text("Deletar"),),
+                PopupMenuItem(value: "Share" ,child: Text("Share"),),
+                ];
+              },
+            ),
+          ],
         ),
         body: _body());
   }
@@ -23,5 +41,23 @@ class CarroPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: Image.network(carro.urlFoto),
     );
+  }
+
+  void _onClickMapa() {}
+
+  void _onClickVideo() {}
+
+  _onClickPopupMenu(String value) {
+    switch(value) {
+      case "Editar":
+        print("Editar !!!");
+        break;
+      case "Deletar":
+        print("Deletar !!!");
+        break;
+      case "Share":
+        print("Share !!!");
+        break;
+    }
   }
 }
