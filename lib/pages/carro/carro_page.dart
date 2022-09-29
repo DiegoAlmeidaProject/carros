0cf4358b3929cceff85dcd06b91e4799793774df
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-09-29 12:32:01 -0300

    Carro Detalhes - Revisão Futures Stream Bloc

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -1,19 +1,35 @@
+import 'package:carros/pages/carro/loripsum_api.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/text.dart';
 import 'carro.dart';
 
-class CarroPage extends StatelessWidget {
+class CarroPage extends StatefulWidget {
+
   Carro carro;
 
   CarroPage(this.carro);
 
   @override
+  State<CarroPage> createState() => _CarroPageState();
+}
+
+class _CarroPageState extends State<CarroPage> {
+  final _loripsumApiBloc = LoripsumBloc();
+
+  @override
+  void initState(){
+    super.initState();
+
+    _loripsumApiBloc.fetch();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(carro.nome),
+          title: Text(widget.carro.nome),
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.place),
@@ -42,7 +58,7 @@ class CarroPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: ListView (
         children: <Widget>[
-          Image.network(carro.urlFoto),
+          Image.network(widget.carro.urlFoto),
           _bloco1(),
           Divider(),
           _bloco2(),
@@ -58,8 +74,8 @@ class CarroPage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget> [
-                  text(carro.nome, fontSize: 20, bold: true),
-                  text(carro.tipo, fontSize: 16),
+                  text(widget.carro.nome, fontSize: 20, bold: true),
+                  text(widget.carro.tipo, fontSize: 16),
                 ],
               ),
             ),
@@ -83,15 +99,17 @@ class CarroPage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        text(carro.descricao, fontSize: 16, bold: true),
+        text(widget.carro.descricao, fontSize: 16, bold: true),
         SizedBox(height: 20,),
-        text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", fontSize: 16),
-
+        StreamBuilder(
+          stream: _loripsumApiBloc.stream,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator(),);
+              }
+              return text(snapshot.data, fontSize: 16);
+            },
+        ),
       ],
     );
   }
@@ -118,5 +136,12 @@ class CarroPage extends StatelessWidget {
   }
 
   void _onClickShare() {
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _loripsumApiBloc.dispose();
   }
 }
