c09a5cfbfbba0b92a86a71ba7dbbcd04ee0ea50b
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-09-30 17:46:43 -0300

    Cache com banco de dados

--- lib/pages/carros/carros_bloc.dart
+++ lib/pages/carros/carros_bloc.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:carros/pages/carros/carros_api.dart';
 import 'package:carros/pages/carros/simple_bloc.dart';
+import 'package:carros/pages/favoritos/carro_dao.dart';
+import 'package:carros/utils/network.dart';
 import 'carro.dart';
 
 class CarrosBloc extends SimpleBloc<List<Carro>> {
@@ -11,9 +13,19 @@ class CarrosBloc extends SimpleBloc<List<Carro>> {
 
   Future<List<Carro>> fetch(String tipo) async {
     try {
+
+      bool networkingOn = await isNetworkOn();
+
+      if (! networkingOn) {
+        List<Carro> carros = await CarroDAO().findAllByTipo(tipo);
+
+        _streamControler.add(carros);
+
+        return carros;
+      }
+
       List<Carro> carros = await CarrosApi.getCarros(tipo);
       _streamControler.add(carros);
-
       return carros;
 
     } catch (e) {
